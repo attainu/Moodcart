e1df3fe610772b810dd34301b5701baa147384f4
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2020-08-19 14:26:31 +0530

    new updates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Container from "./Pages/LoginPage/Container/Container";
 import { Route, Redirect, Switch } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import ProfilePage from "./Pages/profilePage";
-import BookmarkPage from "./Pages/BookmarkPage/bookmark";
+import BookmarkPage from "./Pages/BookmarkPage/BookMark";
 import CountryNewsPage from "./Pages/NewsByCountryPage";
 import OneNewsPage from "./Pages/OneNewsPage";
 import SearchPage from "./Pages/SearchPage";
@@ -13,21 +13,21 @@ import CategoryPage from "./Pages/CategoryPage";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Switch>
-        <Route path='/login' component={Container} />
+        <Route path="/login" component={Container} />
         <Route
           exact
-          path='/home'
+          path="/home"
           render={(routeProps) => <HomePage {...routeProps} />}
         />
-        <Route exact path='/profile' component={ProfilePage} />
-        <Route exact path='/bookmark' component={BookmarkPage} />
-        <Route exact path='/Search/:searchQuery' component={SearchPage} />
-        <Route exact path='/category/:category' component={CategoryPage} />
-        <Route exact path='/country/:country' component={CountryNewsPage} />
-        <Route exact path='/snews/:snewsid' component={OneNewsPage} />
-        <Redirect to='/home' />
+        <Route exact path="/profile" component={ProfilePage} />
+        <Route exact path="/bookmark" component={BookmarkPage} />
+        <Route exact path="/Search/:searchQuery" component={SearchPage} />
+        <Route exact path="/category/:category" component={CategoryPage} />
+        <Route exact path="/country/:country" component={CountryNewsPage} />
+        <Route exact path="/snews/:snewsid" component={OneNewsPage} />
+        <Redirect to="/home" />
       </Switch>
     </div>
   );
